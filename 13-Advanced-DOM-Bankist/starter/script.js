8ee6cc641a6bb4f3bbd9ad812e96874902c11b18
Ifti-Alamin <iftialmin10@gmail.com>
2023-12-15 01:49:31 +0600

    Folder 13 File 19

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -480,7 +480,6 @@ headerObserver.observe(header);
 const revealSection = function (entries, observer) {
   //destructuring
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
 
@@ -501,5 +500,86 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 creating a new array*/
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  //section.classList.add('section--hidden');
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+// here we select all the element specially which have data-src attribute
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  // guard clause
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+// Slider
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+// const slider = document.querySelector('.slider');
+
+// slider.style.transform = 'scale(0.3) translateX(-800px)';
+// // translateX() means start from left position
+// slider.style.overflow = 'visible';
+
+// s = slide num, i = index num
+// transform is a css attribute
+// translateX will basically move them to next position 100%
+// one single slide or image is 100%
+// second image start from 100%, second image start from 200% .......
+// slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`)); // first index is 0
+// 0%, 100%, 200%, 300%
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+// Next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
